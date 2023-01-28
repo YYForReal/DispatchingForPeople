9b46805b848cbe095bacf94d27fb0504d0b6ebfc
友艺 <2572082773@qq.com>
2023-01-28 23:24:28 +0800

    增加随机生成数据并模拟

--- src/map.py
+++ src/map.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+
 # 地图节点的数据
 """
      地名     纬度         经度           震级      半径
@@ -12,40 +13,40 @@ import matplotlib.pyplot as plt
 map_nodes = [
     # 汶川县
     {
-       "name": '水磨镇',
-       "x": 103.395026,
-       "y": 30.972311,
-       "magnitude": 8.0,
-       "population": 11935
+        "name": '水磨镇',
+        "x": 103.395026,
+        "y": 30.972311,
+        "magnitude": 8.0,
+        "population": 11935
     },
     {
-       "name": '绵虒镇',
+        "name": '绵虒镇',
         "x": 103.528645,
         "y": 31.351174,
-       "magnitude": 6.3,
-       "population":8606
+        "magnitude": 6.3,
+        "population": 8606
     },
     # 绵竹市
     {
-       "name": '清平镇',
-       "x": 103.986821,
-       "y": 31.563442,
-       "magnitude": 4.4,
-       "population": 20488
+        "name": '清平镇',
+        "x": 103.986821,
+        "y": 31.563442,
+        "magnitude": 4.4,
+        "population": 20488
     },
     {
-       "name": '响岩镇',
-       "x": 104.708695,
-       "y": 32.081993,
-       "magnitude": 6.8,
-       "population": 10589  # 11年末
+        "name": '响岩镇',
+        "x": 104.708695,
+        "y": 32.081993,
+        "magnitude": 6.8,
+        "population": 10589  # 11年末
     },
     {
         "name": '马角镇',
         "x": 105.008102,
         "y": 32.081993,
-        "magnitude": 6.6    ,
-        "population": 21217 # 11年末
+        "magnitude": 6.6,
+        "population": 21217  # 11年末
     },
 
 ]
@@ -53,27 +54,38 @@ map_nodes = [
 # 补给站
 supply_node = [
     {
-
+        "name": '水磨镇',
+        "x": 103.395026,
+        "y": 30.972311,
+        "robot_num": 1,
+        "material": {
+            "food": 11935
+        }
     }
 ]
 
 random_node_list = []
-
+random_node_supple_list = []
 # 随机生成的节点数据
-for i in range(ord('a'),ord('z')+1):
+for i in range(ord('a'), ord('z') + 1):
     name = chr(i)
-    x = random.uniform(0,90)
-    y = random.uniform(0,180)
-    magnitude = random.uniform(1,8)
-    population = random.randint(1,100)
-    random_node_list.append({
-        "name":name,
-        "x":x,
-        "y":y,
-        "magnitude":magnitude,
-        "population":population
-    })
-
+    x = round(random.uniform(0, 90),3)
+    y = round(random.uniform(0, 180),3)
+    magnitude = round(random.uniform(1, 8),2)
+    population = round(random.uniform(1, 100),2)
+    is_supple = True if random.random() < 0.2 else False
+    new_node = {
+        "name": name,
+        "x": x,
+        "y": y,
+        "magnitude": magnitude,
+        "population": population,
+        "is_supple": is_supple,
+    }
+    if (is_supple):
+        random_node_supple_list.append(new_node)
+    else:
+        random_node_list.append(new_node)
 
 if __name__ == '__main__':
     # 设置字体的属性
@@ -83,15 +95,24 @@ if __name__ == '__main__':
     plt.xlabel("纬度")
     plt.ylabel("经度")
 
-    # 设置绘制的列表
+    # 设置绘制的列表1
     show_nodes = random_node_list
-    # 绘制 受灾点的散点图
+    # 绘制 受灾点的散点图 数据 s 大小 c 颜色 marker 样式
     x = [d["x"] for d in show_nodes]
     y = [d["y"] for d in show_nodes]
-
-    plt.scatter(x, y)
-
+    plt.scatter(x, y, s=16, c="m", marker="s")
+    # 绘制 地名 标注
     for i, d in enumerate(show_nodes):
-        plt.annotate(d["name"], (x[i], y[i]))
+        plt.annotate(d["name"], (x[i] + 0.5, y[i] + 2))
+
+    # 设置绘制的列表2
+    show_nodes = random_node_supple_list
+    # 绘制 受灾点的散点图 数据 s 大小 c 颜色 marker 样式
+    x = [d["x"] for d in show_nodes]
+    y = [d["y"] for d in show_nodes]
+    plt.scatter(x, y, s=28, c="g", marker="p")
+    # 绘制 地名 标注
+    for i, d in enumerate(show_nodes):
+        plt.annotate(d["name"], (x[i] + 0.5, y[i] + 2))
 
     plt.show()
